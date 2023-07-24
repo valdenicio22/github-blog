b375d21dbfceb166c0cf4c314e7421e7f259bcf5
valdenicio22 <valdenicio22@gmail.com>
2023-07-24 10:16:43 +0200

    feat: create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
-import coverImg from '../../assets/cover.svg'
+import logoImg from '../../assets/logo.svg'
 
 export function Header() {
   return (
-    <header>
-      <img src={coverImg} alt="cover" className="w-full" />
+    <header
+      className={`w-full h-[18.5rem] bg-[url('/src/assets/cover.svg')] bg-no-repeat bg-center bg-cover flex items-center justify-center`}
+    >
+      <img src={logoImg} alt="cover" className="mb-12" />
     </header>
   )
 }
