4c383937261762eaf069657fa0b3eacd0236b084
valdenicio22 <valdenicio22@gmail.com>
2023-07-24 11:48:13 +0200

    feat: create InfoWithIcon component

---
+++ src/components/InfoWithIcon/index.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react'
+import { RegularText } from '../Typography/RegularText'
+
+interface InfoWithIconProps {
+  icon: ReactNode
+  info: string
+}
+
+export function InfoWithIcon({ icon, info }: InfoWithIconProps) {
+  return (
+    <div className="flex gap-2">
+      <span className="w-[1.125rem] h-[1.125rem] text-base-label">{icon}</span>
+      <RegularText
+        color="subtitle"
+        className="flex items-center justify-center"
+      >
+        {info}
+      </RegularText>
+    </div>
+  )
+}
