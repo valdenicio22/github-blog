26b3f361f94aca9980e1c95afb312a69d54bef09
valdenicio22 <valdenicio22@gmail.com>
2023-07-24 11:54:31 +0200

    feat: add post list section

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,9 +1,36 @@
+import { Post } from '../../components/Post'
 import { Profile } from '../../components/Profile'
+import { RegularText } from '../../components/Typography/RegularText'
+import { TitleText } from '../../components/Typography/TitleText'
 
 export function Home() {
   return (
-    <div className="layout-container">
+    <>
       <Profile />
-    </div>
+      <section className="flex flex-col my-[4.5rem]">
+        <div className="flex items-center justify-between mb-3">
+          <TitleText color="subtitle" size="sm">
+            Publicações
+          </TitleText>
+          <RegularText color="span" size="sm">
+            6 publicações
+          </RegularText>
+        </div>
+        <input
+          type="text"
+          placeholder="Buscar Conteúdo"
+          className={`
+          w-full py-3 px-4 rounded-md bg-base-input border border-base-border placeholder:text-base-label
+          outline-none focus:border-brand-blue`}
+        />
+        <div className="grid grid-cols-2 gap-8 mt-12">
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+      </section>
+    </>
   )
 }
